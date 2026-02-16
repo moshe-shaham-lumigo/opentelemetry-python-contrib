d17d1c44396fe91d69a09961c357608f65e9f836
Moshe Shaham <moshes@lumigo.io>
2026-02-16 13:59:12 +0200

    .

--- instrumentation/opentelemetry-instrumentation-botocore/src/opentelemetry/instrumentation/botocore/extensions/__init__.py
+++ instrumentation/opentelemetry-instrumentation-botocore/src/opentelemetry/instrumentation/botocore/extensions/__init__.py
@@ -35,6 +35,7 @@ _KNOWN_EXTENSIONS = {
     "bedrock-runtime": _lazy_load(".bedrock", "_BedrockRuntimeExtension"),
     "dynamodb": _lazy_load(".dynamodb", "_DynamoDbExtension"),
     "events": _lazy_load(".events", "_EventsExtension"),
+    "kinesis": _lazy_load(".kinesis", "_KinesisExtension"),
     "lambda": _lazy_load(".lmbd", "_LambdaExtension"),
     "secretsmanager": _lazy_load(
         ".secretsmanager", "_SecretsManagerExtension"
